6cc4426c2aa27902b55e1a6931f51497c726ab65
Prem Raj Anand <108508635+Premraj-87@users.noreply.github.com>
2024-06-25 18:20:02 +0000

    container config done

---
+++ js-conversionOperation.js
@@ -0,0 +1,26 @@
+// let score = "prem"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueNumber = Number(score) 
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+// "33" => 33
+//" 33abs" => NaN
+// true => 1; False => 0 
+
+// let isLoggedIn = 1
+
+// let booleanIsLoogedIn = Boolean(isLoggedIn)
+// console.log(isLoggedIn)
+
+// 1 => true: 0 => false
+// "" => false 
+// "prem" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
