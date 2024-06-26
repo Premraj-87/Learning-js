b44a5cee55ff2000b54ceca8d513b2e59c27d4d5
Prem Raj Anand <108508635+Premraj-87@users.noreply.github.com>
2024-06-26 07:49:16 +0000

    container config done

--- Js-basic/datatype-summary.js
+++ Js-basic/datatype-summary.js
@@ -1,2 +1,36 @@
 // Primitive 
+// 7 types : String , Number , Boolean ,null , undefined, Symbol, BigInt
+
+const scope  = 100
+const  scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null 
+let userEmail; //undefined
+
+const id = Symbol ('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 342423422342423n
+
+
+// Reference (Non primitive)
+// Array , Objects ,Functions
+
+const heros = [ "Ironman", "Thor", " Spiderman"]
+let myObj = {
+    name: "Prem ",
+    age: 22,
+}
+
+const myfunction = function (){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof id);
+console.log(typeof myfunction); 
+
 
