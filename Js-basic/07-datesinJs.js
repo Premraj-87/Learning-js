7e4bfbac6d86b379bcde341d73b78f1cb832e663
Prem Raj Anand <108508635+Premraj-87@users.noreply.github.com>
2024-06-29 08:24:43 +0000

    container commit done

---
+++ Js-basic/07-datesinJs.js
@@ -0,0 +1,17 @@
+// Dates 
+
+let myDate = new Date()
+
+// console.log(myDate.toString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreateDate = new Date(2023, 0 , 23); 
+let myCreateDate = new Date("01-14-2024"); 
+// console.log(myCreateDate.toDateString());
+
+let myTimeStamps = Date.now()
+
+console.log(myTimeStamps);
+console.log(myCreateDate.getTime() );
