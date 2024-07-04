9a4eb7a52b56456669ab437320a1588c87e97b35
Prem Raj Anand <108508635+Premraj-87@users.noreply.github.com>
2024-07-04 12:27:04 +0000

    container commit

--- 02-basics/04-objects.js
+++ 02-basics/04-objects.js
@@ -54,8 +54,39 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-console.log(tinderUser.hasOwnProperty("name"));
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty("name"));
+
+// object destructuring
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "Prem Anand"
+}
+// course.coursename
+
+const { courseInstructor: instructor } = course
+console.log(instructor);
+
+const navbar = ({ company }) => {
+
+}
+navbar(company = "prem")
+
+//API concepts
+// {
+
+//     "name": "prem",
+//     "coursename": "js in hindi",
+//     "price": "999",
+// } //json object
+
+// sometime api return array of object
+[{},
+{},
+{},
+    {}
+]
