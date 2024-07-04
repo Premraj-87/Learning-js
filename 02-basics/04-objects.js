abbec3b1c80738b54c627095f9d55d0eb538476c
Prem Raj Anand <108508635+Premraj-87@users.noreply.github.com>
2024-07-04 12:01:37 +0000

    container comit

--- 02-basics/04-objects.js
+++ 02-basics/04-objects.js
@@ -23,6 +23,39 @@ const regularUser = {
 
 const obj1 = { 1: "a", 2: "b" };
 const obj2 = { 3: "a", 4: "b" };
+const obj4 = { 5: "a", 6: "b" };
 
-const obje3 = Object.assign({}, obj1, obj2); //merge the object
-console.log(obje3);
+// const obje3 = Object.assign({}, obj1, obj2); //merge the object
+const obj3 = { ...obj1, ...obj2, ...obj4 }; //merge the object using spread operator
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        name: "Prem",
+        age: 36,
+        location: "Ranchi",
+        isLoggedIn: false
+    },
+    {
+        id: 2,
+        name: "Anand",
+        age: 36,
+        location: "Ranchi",
+        isLoggedIn: true
+    },
+    {
+        id: 3,
+        name: "Raj",
+        age: 36,
+        location: "Ranchi",
+        isLoggedIn: false
+    }
+]
+
+users[1].email
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty("name"));
