a62d5dc62993ecde49b5e81f89b8b7e071ec794c
Prem Raj Anand <108508635+Premraj-87@users.noreply.github.com>
2024-07-26 04:09:40 +0000

    commit

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -21,10 +21,33 @@ function addTwoNumbers(a, b) {
 // const result = addTwoNumbers(2, 3);
 // console.log("Result", result);
 
-function loginUserMessage(username = "sam") {
-    if (!username || username === "") {
-        return "Please provide a username";
-    }
-    result`${username} Just logged in`
+// function loginUserMessage(username = "sam") {
+//     if (!username || username === "") {
+//         return "Please provide a username";
+//     }
+//     result`${username} Just logged in`
+// }
+// console.log(loginUserMessage("Pedro")) 
+
+function calculateCartPrice(val1, val2, ...num1) {
+
+    return num1
 }
-console.log(loginUserMessage("Pedro"))
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "Premrajanand",
+    prices: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.prices}`);
+}
+// handleObject(user);
+handleObject({ username: "Prem", prices: 200 });
+
+const myNewArray = [200, 400, 100, 600]
+function returnSecondValue(getArray) {
+    return getArray[1];
+}
+console.log(returnSecondValue(myNewArray)); 
