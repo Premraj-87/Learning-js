003d2d19d2c1f9c4fdf239b4ae0ccb3d0feeed07
Prem Raj Anand <108508635+Premraj-87@users.noreply.github.com>
2024-07-26 04:51:11 +0000

    commit

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -10,4 +10,38 @@ if (true) {
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+function one() {
+    const username = "Prem";
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    // two()
+}
+// one()
+if (true) {
+    const username = "hitesh"
+    if (username === "Prem") {
+        const website = "youtube "
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+// ++++++++++++ interesting  +++++++++++++
+
+console.log(addone(5))
+function addone(num) {
+    return num + 1;
+
+
+}
+
+addtwo(5)
+const addtwo = function (num) {
+    return num + 2;
+}
